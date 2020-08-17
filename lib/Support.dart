d06b9d5e96425f29f3f65d2f32de34450c71a3fe
Ivan <gutehaltung@gmail.com>
2020-08-17 21:13:03 +0300

    Created stream for ChildrenList

--- lib/Support.dart
+++ lib/Support.dart
@@ -7,25 +7,31 @@ import 'package:get_it/get_it.dart';
 import 'package:rxdart/rxdart.dart';
 
 class SL {
-  List<EmployeesData> get allEmployee => Hive.box<EmployeesData>(Boxes.employeesBox).values.toList();
+//  List<EmployeesData> get allEmployee => Hive.box<EmployeesData>(Boxes.employeesBox).values.toList();
+//
+//  final _employee = BehaviorSubject<EmployeesData>();
+//  Stream get streamEmployee$ => _employee.stream;
+//  EmployeesData get currentEmployee => _employee.value;
+//  set setEmployee(EmployeesData employee) => _employee.add(employee);
+//
+//
+//  final _allEmployee = BehaviorSubject<List<EmployeesData>>.seeded(Hive.box<EmployeesData>(Boxes.employeesBox).values.toList());
+//  Stream get streamAllEmployee$ => _allEmployee.stream;
+//  List<EmployeesData> get currentListOfEmployee => _allEmployee.value;
+//  set setListEmployee(List<EmployeesData> newListOfEmployee) => _allEmployee.add(newListOfEmployee);
+//  set addEmployee(EmployeesData newEmployee) => Hive.box<EmployeesData>(Boxes.employeesBox).add(newEmployee);
 
-  final _employee = BehaviorSubject<EmployeesData>();
+  final _childrenList = BehaviorSubject<List<ChildrenData>>.seeded(Hive.box<ChildrenData>(Boxes.childrenBox).values);
+  Stream get streamChildrenList => _childrenList.stream;
+  List<ChildrenData> get currentChildrenList => _childrenList.value;
+  set setChildrenList(List<ChildrenData> newChildrenList) => _childrenList.add(newChildrenList);
+  set addChildren(ChildrenData newChild) => Hive.box<ChildrenData>(Boxes.childrenBox).add(newChild);
 
-  Stream get streamEmployee$ => _employee.stream;
-
-  EmployeesData get currentEmployee => _employee.value;
-
-  set setEmployee(EmployeesData employee) => _employee.add(employee);
-
-  final _allEmployee = BehaviorSubject<List<EmployeesData>>.seeded(Hive.box<EmployeesData>(Boxes.employeesBox).values.toList());
-
-  Stream get streamAllEmployee$ => _allEmployee.stream;
-
-  List<EmployeesData> get currentListOfEmployee => _allEmployee.value;
-
-  set setListEmployee(List<EmployeesData> newListOfEmployee) => _allEmployee.add(newListOfEmployee);
-
-  set addEmployee(EmployeesData newEmployee) => Hive.box<EmployeesData>(Boxes.employeesBox).add(newEmployee);
+  void filter(String searchingText) {
+    _childrenList.add(Hive.box<ChildrenData>(Boxes.childrenBox)
+        .values
+        .where((child) => child.name.contains(searchingText) || child.surName.contains(searchingText) || child.patronymic.contains(searchingText)));
+  }
 }
 
 String monthFromNumber(DateTime dateTime) {
--- lib/pages/ChildrenList.dart
+++ lib/pages/ChildrenList.dart
@@ -4,38 +4,76 @@ import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:employees_children/Support.dart';
 
-class ChildrenList extends StatelessWidget {
+class ChildrenList extends StatefulWidget {
+  @override
+  _ChildrenListState createState() => _ChildrenListState();
+}
+
+class _ChildrenListState extends State<ChildrenList> {
+  Iterable<ChildrenData> childrenBox;
+  TextEditingController _filter = TextEditingController();
+
+//  List<ChildrenData> filter({List<ChildrenData> listOfChildren, String searchingText}) {
+//    print(searchingText);
+//    return listOfChildren.where((child) => child.name.contains(searchingText) || child.surName.contains(searchingText) || child.patronymic.contains(searchingText));
+//  }
+//
+//  @override
+//  void initState() {
+//    childrenBox = Hive.box<ChildrenData>(Boxes.childrenBox)
+//        .values
+//        .where((child) => child.name.contains(_filter.text) || child.surName.contains(_filter.text) || child.patronymic.contains(_filter.text));
+//    super.initState();
+//  }
+
   @override
   Widget build(BuildContext context) {
+    print('rebuild ChildrenList');
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         title: const Text('The list of children'),
         actions: [_ButtonAddChildren()],
       ),
-      body: ValueListenableBuilder(
-        valueListenable: Hive.box<ChildrenData>(Boxes.childrenBox).listenable(),
-        builder: (context, Box<ChildrenData> box, _) {
-          if (box.values.length == 0) return Center(child: Text('No children in the list'));
-          return ListView.builder(
-            itemCount: box.values.length,
-            itemBuilder: (context, index) {
-              ChildrenData theChild = box.getAt(index);
-              return Card(
-                elevation: 0,
-                child: Column(
-                  children: <Widget>[
-                    ListTile(
-                      title: Text('${theChild.surName} ${theChild.name}'),
-//                      subtitle: Text('${theChild.birthdate.year}-${theChild.birthdate.month}-${theChild.birthdate.day}'),
-                      onTap: () => Navigator.of(context).pushNamed(RouteNames.showChild, arguments: theChild),
-                    ),
-                  ],
-                ),
-              );
-            },
-          );
-        },
+      body: Column(
+        children: [
+          Expanded(
+            child: ValueListenableBuilder(
+              valueListenable: Hive.box<ChildrenData>(Boxes.childrenBox).listenable(), //Using Hive as state management.
+              builder: (context, Box<ChildrenData> box, _) {
+                if (box.values.length == 0) return Center(child: Text('No children in the list')); //Return a text if there are no records.
+                return ListView.builder(
+                  itemCount: box.length,
+                  itemBuilder: (context, index) {
+                    ChildrenData theChild = box.getAt(index);
+                    return Card(
+                      elevation: 0,
+                      child: Column(
+                        children: <Widget>[
+                          ListTile(
+                            title: Text('${theChild.surName} ${theChild.name}'),
+                            subtitle: Text(monthFromNumber(theChild.birthdate)),
+                            onTap: () => Navigator.of(context).pushNamed(RouteNames.showChild, arguments: theChild),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(15, 1, 65, 1),
+            //TODO: create a filtration of all records.
+            child: TextFormField(
+//              controller: _filter,
+              maxLength: 50,
+              decoration: const InputDecoration(hintText: 'Matches in name or surname', labelText: 'Searching', hintStyle: TextStyle(fontSize: 15)),
+              onChanged: (text) => print(text),
+            ),
+          )
+        ],
       ),
       floatingActionButton: IconButton(
         icon: Icon(Icons.add_circle),
